fc0b43d83710fb0b10fef01621ded41a28046839
DuckQuick <1322740180@qq.com>
2019-06-08 12:29:26 +0800

    v1.1202
    v1.1202

--- duckquick/js/message.js
+++ duckquick/js/message.js
@@ -1,3 +1,12 @@
+const Sys = {};
+const ua = navigator.userAgent.toLowerCase();
+let s;
+(s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+    (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+            (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
+                (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
+
 /*
 function check() {
     let name = document.forms["message-form"]["name"].value;
@@ -20,49 +29,44 @@ function check() {
     }
 }
 */
-function CheckRight(input) {
 
-    if(input.type === 'email') {
-        if (!input.value.match(/^\w+@[a-zA-Z0-9]{2,10}(?:\.[a-z]{2,4}){1,3}$/)) {
-
-        }
-        else {
-
-        }
-    }
-}
-
+/**
+ * @return {boolean}
+ */
 function EmailAdded(input) {
-    let ipt=input.value;
-    let elt=document.getElementById("EmailList");
-    elt.DataSource="";
-    let k = "@";
-    let EmailList = [
-        {value: "qq.com"},
-        {value: "163.com"},
-        {value: "126.com"},
-        {value: "sohu.com"},
-        {value: "yahoo.com"},
-        {value: "hotmail.com"},
-        {value: "gmail.com"},
-        {value: "googlemail.com"},
-        {value: "msn.com"},
-    ];
-    if(!ipt.match(/@/)){
-        k = "@";
-    }
-    else {
-        k="";
-        EmailList = [
-            {value:""},
+    if (Sys.chrome) {
+        let ipt = input.value;
+        let elt = document.getElementById("EmailList");
+        elt.DataSource = "";
+        let k = "@";
+        let EmailList = [
+            {value: "qq.com"},
+            {value: "163.com"},
+            {value: "126.com"},
+            {value: "sohu.com"},
+            {value: "yahoo.com"},
+            {value: "hotmail.com"},
+            {value: "gmail.com"},
+            {value: "googlemail.com"},
+            {value: "msn.com"},
         ];
-    }
-    for(let i=0;i<EmailList.length;i++){
-        let List = EmailList[i];
-        let opt=document.createElement("option");
-        opt.setAttribute("lable",ipt + k + List.value);
-        opt.setAttribute("value",ipt + k + List.value);
-        elt.appendChild(opt);
-    }
+        if (!ipt.match(/@/)) {
+            k = "@";
+        } else {
+            k = "";
+            EmailList = [
+                {value: ""},
+            ];
+        }
+        for (let i = 0; i < EmailList.length; i++) {
+            let List = EmailList[i];
+            let opt = document.createElement("option");
+            opt.setAttribute("lable", ipt + k);
+            opt.setAttribute("value", ipt);
+            elt.appendChild(opt);
+            opt.label += List.value;
+            opt.value += k + List.value;
+        }
+    } else return false;
 }
 
