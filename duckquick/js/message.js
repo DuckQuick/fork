88e7e882f1779f655c11e86b5815da783b1e5b2e
DuckQuick <1322740180@qq.com>
2019-06-09 15:47:10 +0800

    v1.1204 added
    finish the "email-input" input-box

--- duckquick/js/message.js
+++ duckquick/js/message.js
@@ -7,73 +7,10 @@ let s;
             (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
                 (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
 
-/*
-function check() {
-    let name = document.forms["message-form"]["name"].value;
-    let email = document.forms["message-form"]["email"].value;
-    let contents = document.forms["message-form"]["contents"].value;
-    if(name == null || name === ""){
-        alert("君为无名无姓之人？");
-        return false;
-    }
-    else if(!email.match(/^\w+@[a-zA-Z0-9]{2,10}(?:\.[a-z]{2,4}){1,3}$/)){
-        alert("警察叔叔查户口，请填写正确格式的邮箱");
-        return false;
-    }
-     else if(contents.length < 15){
-        alert("哎呀，不够十五字呢");
-        return false;
-    }
-    else {
-        alert("提交成功");
-    }
-}
-*/
 
-/**
- * @return {boolean}
- */
-/*function EmailAdded(input) {
-    if (Sys.chrome) {
-        let ipt = input.value;
-        let elt = document.getElementById("EmailList");
-        let k = "@";
-        let EmailList = [
-            {value: "qq.com"},
-            {value: "163.com"},
-            {value: "126.com"},
-            {value: "sohu.com"},
-            {value: "yahoo.com"},
-            {value: "hotmail.com"},
-            {value: "gmail.com"},
-            {value: "googlemail.com"},
-            {value: "msn.com"},
-        ];
-        if (!ipt.match(/@/)) {
-            k = "@";
-        } else {
-            k = "";
-            EmailList = [
-                {value: ""},
-            ];
-        }
-        for (let i = 0; i < EmailList.length; i++) {
-            let List = EmailList[i];
-            let opt = document.createElement("option");
-            opt.setAttribute("lable", "");
-            opt.setAttribute("value", "");
-            elt.appendChild(opt);
-            if(opt.value) opt.value="";
-            if(opt.label) opt.label="";
-            opt.label += k + List.value;
-            opt.value += ipt + k + List.value;
-        }
-    }
-}
-*/
-function EmailAdded(input) {
+function emailAdded(input) {
     let ipt=input.value;
-    let elt = document.getElementById("EmailList");
+    let elt = document.getElementById("emailList");
     elt.style.visibility = "visible";
     elt.innerHTML = "";
     const EmailList = [
@@ -96,10 +33,15 @@ function EmailAdded(input) {
             }else {
                 p.innerHTML = ipt + "@" + List.value;
             }
+            p.onclick = function () {
+                input.setAttribute("value",p.innerHTML);
+                document.getElementById("emailInput").value=p.innerHTML;
+                elt.style.visibility = "hidden";
+            };
             elt.appendChild(p);
-            if(ipt.match(/@(.+)|[^a-zA-Z0-9@]/)){
-                elt.innerHTML = "";
             }
         }
+    if(ipt.match(/@(.+)|[^a-zA-Z0-9@]/)){
+        elt.innerHTML = "";
     }
 }
